5a3871cee745bb07466b616a2af5c7719b540801
Hyojeong <79013520+world-dv@users.noreply.github.com>
2023-07-08 22:59:08 +0900

    Create Luck Balance.py

---
+++ python/Greedy/Luck Balance.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'luckBalance' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. 2D_INTEGER_ARRAY contests
+#
+
+def luckBalance(k, contests):
+    # Write your code here
+    a = sorted([i for i, j in contests if j], reverse=True)
+    a = sum(a[:k]) - sum(a[k:])
+    a += sum([i for i, j in contests if not j])
+    return a
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    contests = []
+
+    for _ in range(n):
+        contests.append(list(map(int, input().rstrip().split())))
+
+    result = luckBalance(k, contests)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
