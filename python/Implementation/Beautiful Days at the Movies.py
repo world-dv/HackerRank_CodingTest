0c08477c8ebd7069c7229f384808946bb535d9fe
Hyojeong <79013520+world-dv@users.noreply.github.com>
2023-12-08 20:01:00 +0900

    Create Beautiful Days at the Movies.py

---
+++ python/Implementation/Beautiful Days at the Movies.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'beautifulDays' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER i
+#  2. INTEGER j
+#  3. INTEGER k
+#
+
+def beautifulDays(i, j, k):
+    arr = dict()
+    for day in range(i, j+1):
+        beautiful = abs(day - int(str(day)[::-1])) / k
+        if beautiful not in arr:
+            arr[beautiful] = 1
+        else:
+            arr[beautiful] += 1
+    return sum([arr.get(z) for z in arr if float.is_integer(z)])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    i = int(first_multiple_input[0])
+
+    j = int(first_multiple_input[1])
+
+    k = int(first_multiple_input[2])
+
+    result = beautifulDays(i, j, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
