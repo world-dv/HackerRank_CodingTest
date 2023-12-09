671d4376d7e25052a2d2cb5567c24c48900ef714
Hyojeong <79013520+world-dv@users.noreply.github.com>
2023-12-09 12:55:51 +0900

    Create Viral Advertising.py

---
+++ python/Implementation/Viral Advertising.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'viralAdvertising' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def viralAdvertising(n):
+    shared = 5
+    linked = 2
+    answer = linked
+    for i in range(0, n - 1):
+        shared = (shared // 2) * 3
+        linked = shared // 2
+        answer += linked
+    return answer
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
