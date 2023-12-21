53540b158d0f73887f40685cdbefcec6aae4bc4c
Hyojeong <79013520+world-dv@users.noreply.github.com>
2023-12-21 22:26:42 +0900

    Create Alternating Characters.py

---
+++ python/Strings/Alternating Characters.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'alternatingCharacters' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def alternatingCharacters(s):
+    start = 0
+    count = 0
+    for i in s:
+        if start == 0:
+            start = i
+            continue
+        if start == i:
+            count += 1
+        if start != i:
+            start = i
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = alternatingCharacters(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
