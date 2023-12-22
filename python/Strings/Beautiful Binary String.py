5fe19855602ac4b689eee87ac7cf231b76dfb43a
Hyojeong <79013520+world-dv@users.noreply.github.com>
2023-12-22 14:40:58 +0900

    Create Beautiful Binary String.py

---
+++ python/Strings/Beautiful Binary String.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'beautifulBinaryString' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING b as parameter.
+#
+
+def beautifulBinaryString(b):
+    return (len(b) - len(b.replace('010', ''))) // 3
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    b = input()
+
+    result = beautifulBinaryString(b)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
