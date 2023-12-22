73ea69f95a529a88268bc41d6d587a301652c21d
Hyojeong <79013520+world-dv@users.noreply.github.com>
2023-12-22 16:36:49 +0900

    Create Gemstones.py

---
+++ python/Strings/Gemstones.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gemstones' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING_ARRAY arr as parameter.
+#
+
+def gemstones(arr):
+    ar = dict()
+    total = []
+    for i in arr:
+        total += list(set(i))
+    for i in total:
+        ar[i] = total.count(i)
+    answer = [i for i in ar if ar[i] == len(arr)]
+    return len(answer)
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr_item = input()
+        arr.append(arr_item)
+
+    result = gemstones(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
