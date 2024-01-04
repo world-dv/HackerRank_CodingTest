f5069a20f5973a4334a3941875a5e3c2e654f77d
Hyojeong <79013520+world-dv@users.noreply.github.com>
2024-01-04 22:32:46 +0900

    Create Migratory Birds.py

---
+++ python/Implementation/Migratory Birds.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):
+    dic = dict()
+    a = 0
+    for i in list(set(arr)):
+        dic[i] = arr.count(i)
+        if a < dic[i]:
+            a = dic[i]
+    ar = [i for i in dic if dic[i] == a]
+    return ar[0]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
