812f4e777de38a5f54f4ba695f2d88b89905f1fa
Hyojeong <79013520+world-dv@users.noreply.github.com>
2024-01-05 22:25:09 +0900

    Create Minimum Distances.py

---
+++ python/Implementation/Minimum Distances.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'minimumDistances' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def minimumDistances(a):
+    arr = []
+    for i in range(len(a)):
+        if a[i+1:].count(a[i]) >= 1:
+            arr.append(abs(i - (a.index(a[i], i+1))))
+    return min(arr) if arr else -1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = minimumDistances(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
