d5ca7db75fb481a37423b7a0a496fdd11f52268f
Hyojeong <79013520+world-dv@users.noreply.github.com>
2024-01-09 21:07:06 +0900

    Create Jumping on the Clouds: Revisited.py

---
+++ python/Implementation/Jumping on the Clouds: Revisited.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the jumpingOnClouds function below.
+def jumpingOnClouds(c, k):
+    e = 0
+    idx = 0
+    while True:
+        idx = (idx + k) % len(c)
+        if c[idx]:
+            e -= 3
+        else:
+            e -= 1
+        if idx == 0:
+            break
+    return e + 100
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
