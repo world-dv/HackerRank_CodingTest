7a0062c26b6e3e58fb33060beb3cc5499351f291
Hyojeong <79013520+world-dv@users.noreply.github.com>
2024-01-11 21:58:53 +0900

    Create Beautiful Triplets.py

---
+++ python/Implementation/Beautiful Triplets.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'beautifulTriplets' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER d
+#  2. INTEGER_ARRAY arr
+#
+
+def beautifulTriplets(d, arr):
+    answer = 0
+    for i in range(len(arr)):
+        for j in range(i + 1, len(arr)):
+            if arr[j] - arr[i] == d:
+                for k in range(j + 1, len(arr)):
+                    if arr[k] - arr[j] == d:
+                        answer += 1
+    return answer
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = beautifulTriplets(d, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
