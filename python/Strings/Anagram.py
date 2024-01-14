846bbd1dfb5d503f98478863e713689614311174
Hyojeong <79013520+world-dv@users.noreply.github.com>
2024-01-14 23:07:02 +0900

    Create Anagram.py

---
+++ python/Strings/Anagram.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'anagram' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def anagram(s):
+    if len(s) % 2 != 0:
+        return -1
+    a = s[:len(s)//2]
+    b = s[len(s)//2:]
+    for i in a:
+        if i in b:
+            b = b.replace(i, '', 1)
+    return len(b)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = anagram(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
